97a76d70a3499ba4212812d1f7bef2154a4922e6
WWWWYYYYAAAA <767210874@qq.com>
2026-03-23 22:16:45 +0800

    add_smooth_target

--- scripts/simple_move.py
+++ scripts/simple_move.py
@@ -13,6 +13,7 @@ from piper_control import (
     piper_interface,
 )
 
+import numpy as np
 
 def main():
   print(
@@ -45,19 +46,24 @@ def main():
       move_mode=piper_interface.MoveMode.MIT,
   )
 
-  print("resetting gripper")
-  piper_init.reset_gripper(robot)
+  # print("resetting gripper")
+  # piper_init.reset_gripper(robot)
 
   robot.show_status()
-
+  # robot.get_joint_positions()
+  # while 1:
+  #   print(robot.get_joint_positions())
   # First open and close the gripper
-  with piper_control.GripperController(robot) as controller:
-    controller.command_open()
-    time.sleep(2.0)
-    controller.command_close()
-    time.sleep(2.0)
+  # with piper_control.GripperController(robot) as controller:
+  #   controller.command_open()
+  #   time.sleep(2.0)
+  #   controller.command_close()
+  #   time.sleep(2.0)
 
   # Move the arm joints using Mit mode controller.
+  cmd_joint_pos = np.array(robot.get_joint_positions(), dtype=np.float32)
+  # target_joint_pos = np.array([0.5, 0.7, -0.4, 0.2, 0.3, 0.5], dtype=np.float32)
+  target_joint_pos = np.array([0.0, 0.0, -0.0, 0.0, 0.0, 0.0], dtype=np.float32)
   with piper_control.MitJointPositionController(
       robot,
       kp_gains=5.0,
@@ -65,18 +71,22 @@ def main():
       rest_position=piper_control.ArmOrientations.upright.rest_position,
   ) as controller:
     print("moving to position ...")
-    success = controller.move_to_position(
-        [0.5, 0.7, -0.4, 0.2, 0.3, 0.5],
-        threshold=0.01,
-        timeout=5.0,
-    )
+    while 1:
+      success = controller.move_to_position(
+          # [0.5, 0.7, -0.4, 0.2, 0.3, 0.5],
+          cmd_joint_pos.tolist(),
+          threshold=0.05,
+          timeout=0.01,
+      )
+      cmd_joint_pos += np.clip(target_joint_pos-cmd_joint_pos, a_min=-0.02, a_max=0.02)
+      time.sleep(0.01)
     print(f"reached target: {success}")
 
   print("finished, disabling arm.")
   print("WARNING: the arm will power off and drop.")
 
   piper_init.disable_arm(robot)
-  piper_init.disable_gripper(robot)
+  # piper_init.disable_gripper(robot)
   time.sleep(0.5)
   print("done disabling. exiting.")
 
